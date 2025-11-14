5cbb3785c59880ed52704b3dfdb99f7f09bbc3f5
NaveedConeio23 <naveed@coneio.com>
2025-11-14 15:23:11 +0530

    backend url added

--- backend/settings.py
+++ backend/settings.py
@@ -141,7 +141,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
     os.getenv("FRONTEND_URL", ""),  
-    "sellerapp-backend-20xn.onrender.com",
+    "https://sellerapp-backend-20xn.onrender.com",
  
 ]
 
